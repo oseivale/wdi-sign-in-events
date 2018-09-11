7f92d6028a5dc919681212acdd04aadf260d535b
Valerie Osei <valerie.osei@gmail.com>
2018-09-11 15:49:16 -0400

    created a series of event listeners to open and close modal window

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// When the user presses the .signin button, display the modal window
+// When the user presses the .close button, hide the modal window
+// When the user presses the .submit button, add an .error class to both input elements
+// When the user puts their cursor in one of the input fields, remove the .error class
+// (Bonus) Allow the user to click the .modal to close itself
+// Tricky, consider what happens when two elements that are overlapping are both waiting for a click
+
+document.addEventListener('DOMContentLoaded', function(){
+
+var signIn = document.querySelector('.signin');
+var modalWindow = document.querySelector('.modal');
+var close = document.querySelector('.close');
+var submitButton = document.querySelector('submit');
+
+signIn.addEventListener('click', function(){
+  modalWindow.style.display = 'block';
+})
+
+close.addEventListener('click', function(){
+  modalWindow.style.display = 'none';
+})
+
+modalWindow.addEventListener('click', function(){
+  modalWindow.style.display = 'none';
+})
+
+submitButton.createElement('error');
+
+
+
+});
